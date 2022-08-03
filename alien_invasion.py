03b8708c037e2130f83dcdf44e0a529f1f81c09b
newmain-lee <leezgnj@163.com>
2022-08-03 21:15:46 +0800

    chapter 12.3 is completed

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,33 +1,33 @@
 # allien invasion Game
 import sys
 import pygame
-
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
-from pygame.sprite import Group
 import game_function as gf
+
+from alien import Alien
 
 
 def run_game():
     pygame.init()
-    ai_settings = Settings();
+    ai_settings = Settings()
     screen = pygame.display.set_mode(
-        (ai_settings.screen_width,ai_settings.screen_high))
+        (ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     ship = Ship(ai_settings,screen)
     bullets = Group()
+
+    aliens = Group()
+    gf.creat_fleet(ai_settings,screen,ship,aliens)
+
 
     while True:
 
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
-        bullets.update()
 
-        for bullet in bullets.copy():
-            if bullet.rect.bottom <= 0:
-                bullets.remove(bullet)
-        print(len(bullets))
-
-        gf.update_screen(ai_settings,screen,ship,bullets)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
 run_game()
